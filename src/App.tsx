2cfd0744b6631c3087c55bcd8d27a366b8be4149
Denis Klintukhov <35447261+DKlintukhov@users.noreply.github.com>
2025-02-11 11:54:49 +0300

    Fix: default location on page refresh (#12)

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,50 @@
-import { Routes, Route, useNavigate } from 'react-router-dom';
-import { Container } from '@mui/material';
-import { Exercise, ExerciseSetup, Results } from './pages';
-import { useState } from 'react';
-import { SimpleExpression } from './models';
+import { Routes, Route, useNavigate, useLocation } from "react-router-dom";
+import { Container } from "@mui/material";
+import { Exercise, ExerciseSetup, Results } from "./pages";
+import { useEffect, useState } from "react";
+import { SimpleExpression } from "./models";
 
 export function App() {
     const navigate = useNavigate();
+    const location = useLocation();
     const [expressions, setExpressions] = useState<SimpleExpression[]>([]);
     const [timeout, setTimeout] = useState<number>(0);
 
     const exerciseStarted = (timeout: number, expressions: SimpleExpression[]) => {
         setTimeout(timeout);
         setExpressions(expressions);
-        navigate('/exercise');
+        navigate("/exercise");
     }
 
     const exerciseFinished = (duration: number, answers: number[]) => {
         console.log(duration, answers);
-        navigate('/results');
+        navigate("/results");
     }
 
     const finished = () => {
-        navigate('/');
+        navigate("/");
     }
 
+    useEffect(() => {
+        if (location.pathname !== "/") {
+            navigate("/");
+        }
+    }, []);
+
     return (
-        <Container maxWidth='md'
+        <Container maxWidth="md"
             style={{
-                height: '100vh',
-                width: '100vw',
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                overflow: 'hidden'
+                height: "100vh",
+                width: "100vw",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                overflow: "hidden"
             }}>
             <Routes>
-                <Route path='/' element={<ExerciseSetup onStarted={exerciseStarted} />} />
-                <Route path='/exercise' element={<Exercise timeout={timeout} expressions={expressions} onFinished={exerciseFinished} />} />
-                <Route path='/results' element={<Results onFinished={finished} />} />
+                <Route path="/" element={<ExerciseSetup onStarted={exerciseStarted} />} />
+                <Route path="/exercise" element={<Exercise timeout={timeout} expressions={expressions} onFinished={exerciseFinished} />} />
+                <Route path="/results" element={<Results onFinished={finished} />} />
             </Routes>
         </Container>
 
